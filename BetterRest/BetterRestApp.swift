69d1f4ec5630dd8e7a20594a6eee71100a9eab5c
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-02 07:38:11 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Zaid Neurothrone on 2022-10-02.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
